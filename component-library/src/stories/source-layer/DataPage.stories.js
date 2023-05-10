e1bc10299fccb5eec71e9809b8c1aaa5129e284d
Court Cox <82842603+coxco96@users.noreply.github.com>
2023-05-10 00:36:51 -0400

    fix data section metadata

--- component-library/src/stories/source-layer/DataPage.stories.js
+++ component-library/src/stories/source-layer/DataPage.stories.js
@@ -11,16 +11,11 @@ export default {
 
 export const AddSourceAndLayer = {
   args: {
-    title: 'Add some Data',
-    codeBlock: `<todo/>`,
-    content: 'come back to this // this is acpa data',
+    title: 'Add some data (ACPA cleft care locations)',
+    codeBlock: `<MapContainer data={acpa} layerOpts={type: 'circle'} mapOpts={{center: [-95, 41], zoom: 3.1}}/>`,
     data: acpa,
     layerOpts: {
-        type: 'circle',
-        paint: {
-            "circle-radius": 4,
-            "circle-color": "yellow"
-        },
+        type: 'circle'
     },
     mapOpts: {
         center: [-95, 41],
@@ -30,11 +25,10 @@ export const AddSourceAndLayer = {
 }
 
 
-export const RestyleData = {
+export const StyleData = {
     args: {
-      title: 'Restyle',
-      codeBlock: `<todo/>`,
-      content: 'come back to this',
+      title: 'Style your data',
+      codeBlock: `<MapContainer data={acpa} layerOpts={{type: 'circle', paint: {'circle-radius': 9, 'circle-color': 'orange', 'circle-blur': .7, 'circle-opacity': .5, 'circle-stroke-wdith': .5, 'circle-stroke-opacity: .1, 'circle-stroke-color': 'brown'}}} mapOpts={{center: [-95, 41], zoom: 3.1}}/>`,
       data: acpa,
       layerOpts: {
           type: 'circle',
@@ -55,11 +49,10 @@ export const RestyleData = {
     }
   }
 
-  export const FillExtrusion = {
+  export const FillExtrusionExample = {
     args: {
-        title: 'Fill Extrusion Example',
-        codeBlock: `todo`,
-        content: 'adapted from mapbox api example',
+        title: 'Fill Extrusion (from Mapbox Examples)',
+        codeBlock: `<MapContainer data='https://docs.mapbox.com/mapbox-gl-js/assets/indoor-3d-map.geojson' layerOpts={{type: 'fill-extrusion', paint: {'fill-extrution-color': ['get', color'], 'fill-extrusion-height': ['get', 'height'],'fill-extrusion-base': ['get', 'base_height'],'fill-extrusion-opacity': 0.5}}} mapOpts={{center: [-87.61694, 41.86625], zoom: 16.5, pitch: 40, bearing: 30, antialias: true, style: styles.ligh}}/>`,
         data:'https://docs.mapbox.com/mapbox-gl-js/assets/indoor-3d-map.geojson',
         layerOpts: {
             type: 'fill-extrusion',
